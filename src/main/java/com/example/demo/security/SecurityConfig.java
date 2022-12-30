b20dd0b2da011e77b7a0d8a7f8d802a9abc0652e
Darji Aymane <aymanedarji7@gmail.com>
2022-12-30 10:46:37 +0100

    Add spring security classes to the project(JWT)

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.example.demo.security;
+
+import com.example.demo.filter.CustomAuthenticationFilter;
+import com.example.demo.filter.CustomAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @Override
+    protected void configure ( AuthenticationManagerBuilder auth ) throws Exception {
+        System.out.println ("this is configure method inside SecurityConfig with param auth" );
+        auth.userDetailsService (  userDetailsService).passwordEncoder ( bCryptPasswordEncoder );
+    }
+
+    @Override
+    protected void configure ( HttpSecurity http ) throws Exception {
+        System.out.println ("this is configure in Security configure with http param" );
+        CustomAuthenticationFilter customAuthenticationFilter =
+                new CustomAuthenticationFilter(authenticationManagerBean ());
+
+        customAuthenticationFilter.setFilterProcessesUrl ( "/api/login" );
+
+        http.csrf ().disable ();
+        http.sessionManagement ().sessionCreationPolicy ( STATELESS );
+        http.authorizeRequests ().antMatchers (  "/").permitAll ()
+                                .antMatchers(POST, "/api/v1/orders").hasAnyAuthority("ROLE_CLIENT")
+                                .antMatchers(GET, "/api/v1/users/user").hasAnyAuthority("ROLE_CLIENT")
+                                .antMatchers ("/api/v1/users/**").hasAnyAuthority ( "ROLE_ADMIN" )
+                                .antMatchers(POST, "/api/v1/products/addProduct").hasAnyAuthority("ROLE_ADMIN")
+                                .antMatchers (DELETE,"/api/v1/products/product").hasAnyAuthority ( "ROLE_ADMIN" )
+                                .antMatchers ( "/api/v1/products/**").permitAll ()
+        //http.authorizeRequests ().antMatchers (POST, "/api/v1/users/").hasAnyAuthority ( "ROLE_ADMIN" );
+                                .anyRequest ().authenticated ();
+        http.addFilter ( customAuthenticationFilter  );
+        http.addFilterBefore ( new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class );
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean ();
+    }
+}
