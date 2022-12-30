820f71fac3d59aaff5688c5fefa282643da3cdab
Darji Aymane <aymanedarji7@gmail.com>
2022-12-30 10:43:59 +0100

    Create Product entity

---
+++ src/main/java/com/example/demo/domain/Role.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @JsonIgnore
+    @OneToMany (mappedBy = "role")
+    List<Client> client;
+
+}
